f50f2661d5bb90d4b2ab510a8c23204015c24914
Leon <yaroshenko.leonid@gmail.com>
2024-02-01 13:40:07 +0300

    Добавьте файлы проекта.

---
+++ CsharpWPF_HW5/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpWPF_HW5
+{
+    class CalculatorArgs : EventArgs
+    {
+        public int answer = 0;
+    }
+
+    class Calculator
+    {
+        public event EventHandler<CalculatorArgs> Result;
+        public int result { get; private set; } = 0;
+        Stack<int> results = new Stack<int>();
+
+        private void Calculation()
+        {
+            Result.Invoke(this, new CalculatorArgs { answer = result });
+        }
+
+        public void Add(int value)
+        {
+            results.Push(result);
+            result += value;
+            Calculation();
+        }
+
+        public void Sub(int value)
+        {
+            results.Push(result);
+            result -= value;
+            Calculation();
+        }
+
+        public void Mult(int value)
+        {
+            results.Push(result);
+            result *= value;
+            Calculation();
+        }
+
+        public void Div(int value)
+        {
+            results.Push(result);
+            result /= value;
+            Calculation();
+        }
+
+        public void Cancel()
+        {
+            if (results.Count > 0)
+            {
+                result = results.Pop(); ;
+                Calculation();
+            }
+        }
+    }
+}
---
+++ CsharpWPF_HW5/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CsharpWPF_HW5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+            calculator.Result += Calculator_Result;
+        }
+
+        private void Calculator_Result(object sender, CalculatorArgs e)
+        {
+            Answer.Content = e.answer;
+        } 
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputText.Text == string.Empty)
+                Environment.Exit(0);
+
+            bool flag = int.TryParse(InputText.Text, out int value);
+
+            string name = (e.Source as FrameworkElement).Name;
+
+            if (!flag) {
+                MessageBox.Show("Неверно ввели данные");
+                InputText.Text = string.Empty;
+            }
+
+            switch (name)
+            {
+                case "Add":
+                    calculator.Add(value);
+                    break;
+                case "Sub":
+                    calculator.Sub(value);
+                    break;
+                case "Mult":
+                    calculator.Mult(value);
+                    break;
+                case "Div":
+                    calculator.Div(value);
+                    break;
+                case "Cancel":
+                    calculator.Cancel();
+                    break;
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    MessageBox.Show("Ошибка");
+                    InputText.Text = string.Empty;
+                    break;
+            }
+        }
+    }
+}
